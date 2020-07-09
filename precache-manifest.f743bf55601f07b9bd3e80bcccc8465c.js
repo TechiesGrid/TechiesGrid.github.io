ec4be1f41a58739a2a0fce8fd0e2fd80a898cef2
Devansh Suthar <devanshsuthar0612@gmail.com>
2020-07-09 21:10:33 +0530

    Build testing

---
+++ precache-manifest.f743bf55601f07b9bd3e80bcccc8465c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01033e0613aa3897c007a069cb57906",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34b728bb8b9497a6b73c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6f64598ada6d21774385",
+    "url": "/static/js/2.edffc244.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.edffc244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b728bb8b9497a6b73c",
+    "url": "/static/js/main.801bb773.chunk.js"
+  },
+  {
+    "revision": "cde63f8ecf5d72c7a348",
+    "url": "/static/js/runtime-main.a7b0b58f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
